5a911a6628c4f7b764d4422e5538295c97a3772d
Kinshuk Garai <kinshuk.garai@ext.soprasteria.com>
2018-01-27 20:30:24 +0000

    got happy path Currency cuke passing

--- kinitic-acceptance-tests/src/test/java/com/shop/kinitic/acceptance/KiniticShopStepdefs.java
+++ kinitic-acceptance-tests/src/test/java/com/shop/kinitic/acceptance/KiniticShopStepdefs.java
@@ -1,12 +1,24 @@
 package com.shop.kinitic.acceptance;
 
+import static com.jayway.jsonassert.JsonAssert.with;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.withJsonPath;
+import static java.util.stream.Collectors.toList;
 import static javax.ws.rs.client.ClientBuilder.newClient;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 
@@ -18,18 +30,43 @@ public class KiniticShopStepdefs {
     private String responseBody;
 
     @Given("^I invoke the currencies Api endpoint$")
-    public void I_invoke_the_currencies_api_endpoint() throws Throwable {
+    public void iInvokeTheCurrenciesApiEndpoint() throws Throwable {
         final Response response = KINITIC_SHOP_API_BASE_URI.path("/currencies").request().get();
         readResponse(response);
     }
 
     @And("^the response is (\\d+)$")
-    public void responseStatusIs(int status) {
+    public void responseStatusIs(final int status) {
         assertThat(status, is(responseStatus));
+    }
+
+    @And("^the response contains the following currency entries:$")
+    public void theResponseContainsTheFollowingCurrencyEntries(final DataTable dataTable) throws Throwable {
+        final List<Map<String, String>> currencyDetails = dataTable.asMaps();
+
+        final List<String> expectedCurrencyNames = getExpectedValuesFor("name", currencyDetails);
+        final List<String> expectedCurrencyDescription = getExpectedValuesFor("description", currencyDetails);
+
+        assertThat(responseBody, isJson(allOf(
+                withJsonPath("$.currencies", hasSize(currencyDetails.size())),
+                withJsonPath("$.currencies[*].name", hasItems(expectedCurrencyNames.toArray())),
+                withJsonPath("$.currencies[*].description", hasItems(expectedCurrencyDescription.toArray())))
+        ));
+    }
+
+    @And("^the id exists$")
+    public void theIdExists() throws Throwable {
+        with(responseBody)
+                .assertThat("$.currencies[*].id", notNullValue());
     }
 
     private void readResponse(Response response) {
         responseStatus = response.getStatus();
         responseBody = response.readEntity(String.class);
+    }
+
+
+    private List<String> getExpectedValuesFor(String attributeName, List<Map<String, String>> currencyDetails) {
+        return currencyDetails.stream().map(currencyDetail -> currencyDetail.get(attributeName)).collect(toList());
     }
 }
--- kinitic-acceptance-tests/target/test-classes/com/shop/kinitic/acceptance/Currency.feature
+++ kinitic-acceptance-tests/target/test-classes/com/shop/kinitic/acceptance/Currency.feature
@@ -3,3 +3,9 @@ Feature: Tests covering the Currency API.
   Scenario: Should provide all the available currencies for the kinitic shop
     Given I invoke the currencies Api endpoint
     Then the response is 200
+    And the response contains the following currency entries:
+      | name | description     |
+      | USD  | US Dollars      |
+      | GBP  | Pounds Sterling |
+    And the id exists
+
